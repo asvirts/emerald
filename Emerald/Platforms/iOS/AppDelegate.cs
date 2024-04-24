d7e847511bfb09b50cd562ae9266f78e5788e254
Andrew Virts <asvirts@outlook.com>
2024-04-23 19:04:45 -0500

    Add project files.

---
+++ Emerald/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Emerald
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
