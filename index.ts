94a0cfd7e04727f9eff6ee6c8d976efc81c5806a
Andrew Virts <asvirts@outlook.com>
2024-05-22 15:35:39 -0500

    bun version

--- index.ts
+++ index.ts
@@ -1,9 +1,1 @@
-import figlet from "figlet"
-
-const server = Bun.serve({
-  port: 3000,
-  fetch(req) {
-    const body = figlet.textSync("Bun!")
-    return new Response(body)
-  }
-})
+console.log("Bun version =", Bun.version)
